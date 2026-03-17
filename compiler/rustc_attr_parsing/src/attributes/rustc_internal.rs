f0103e52847f974713a898e5512e17ed9a61c569
Dominik Schwaiger <mail@dominik-schwaiger.ch>
2026-03-17 15:08:35 +0100

    apply rustc_no_writable to closures too

--- compiler/rustc_attr_parsing/src/attributes/rustc_internal.rs
+++ compiler/rustc_attr_parsing/src/attributes/rustc_internal.rs
@@ -549,6 +549,7 @@ impl<S: Stage> NoArgsAttributeParser<S> for RustcNoWritableParser {
     const ALLOWED_TARGETS: AllowedTargets = AllowedTargets::AllowList(&[
         Allow(Target::Fn),
         // TODO: allow those kind of functions? is the optimization done here? is this checked?
+        Allow(Target::Closure),
         Allow(Target::Method(MethodKind::Inherent)),
         Allow(Target::Method(MethodKind::TraitImpl)),
         Allow(Target::Method(MethodKind::Trait { body: true })),
